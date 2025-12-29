f6f7beba795d09d6ae353e1614fe55320fabe5d8
EmonHira135923 <emonhossainhira231@gmail.com>
2025-12-30 00:55:35 +0600

    Smart Deals Route Setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,6 +22,97 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const smartdb = client.db("Smart-Deals");
+    const productcollection = smartdb.collection("allproducts");
+    const bidscollection = smartdb.collection("mybids");
+
+    // Product collection router all
+    app.post("/products", async (req, res) => {
+      const query = req.body;
+      const result = await productcollection.insertOne(query);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const cursor = productcollection.find();
+      const allvalues = await cursor.toArray();
+      res.send(allvalues);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productcollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productcollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const newupdatedata = req.body;
+      const updatedata = {
+        $set: newupdatedata,
+      };
+      const options = {};
+      const result = await productcollection.updateOne(
+        query,
+        updatedata,
+        options
+      );
+      res.send(result);
+    });
+
+    // Bids Collection router all
+    app.post("/bids", async (req, res) => {
+      const query = req.body;
+      const result = await bidscollection.insertOne(query);
+      res.send(result);
+    });
+
+    app.get("/bids", async (req, res) => {
+      const cursor = bidscollection.find();
+      const allvalues = await cursor.toArray();
+      res.send(allvalues);
+    });
+
+    app.get("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidscollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const newupdatebids = req.body;
+      const updatebidsdata = {
+        $set: newupdatebids,
+      };
+      const options = {};
+      const result = await bidscollection.updateOne(
+        query,
+        updatebidsdata,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidscollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +126,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Smart Deals!");
 });
 
 app.listen(port, () => {
