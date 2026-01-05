3ece88507f312027b0a7baaf11ad881818c5a811
EmonHira135923 <emonhossainhira231@gmail.com>
2026-01-05 21:41:59 +0600

    Project deploy

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { connectDB } from "./config/db.js";
 const port = process.env.PORT || "3000";
 
 const startServer = async () => {
-  // await connectDB();
+  await connectDB();
 
   app.listen(port, () => {
     console.log(`Smart Deal Management runnings port ${port}`);
